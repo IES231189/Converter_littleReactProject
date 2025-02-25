7434313de7d46156de6f59161a0a6654fc558117
231189alo <231189@ids.upchiapas.edu.mx>
2025-02-24 18:18:53 -0600

    cambios para detectar en el webhook

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
   <>
-  <p>Cambios para detectar en el  webhoock</p>
+  <p>Prueba 4 , webhook</p>
   <h1 className="p-title">Convertidor de numeros</h1>
     <div className="container mt-4">
     <div className="card-list">
