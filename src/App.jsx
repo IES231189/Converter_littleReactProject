baf30a3c019b0039e7a1f0363d8768c9169634dc
231189alo <231189@ids.upchiapas.edu.mx>
2025-02-25 15:11:26 -0600

    prueba 3

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
   return (
   <>
-  <p>Prueba 6 , webhook</p>
+  <p>Prueba 8 , webhook</p>
   <h1 className="p-title">Convertidor de numeros</h1>
     <div className="container mt-4">
     <div className="card-list">
